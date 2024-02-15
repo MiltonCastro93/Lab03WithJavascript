20828f512a5bed12f12f56167224aa219c198a08
Milton Denis Castro <159483393+MiltonCastro93@users.noreply.github.com>
2024-02-15 15:26:59 -0300

    Update mostrar-ocultar2.js

--- js/mostrar-ocultar2.js
+++ js/mostrar-ocultar2.js
@@ -1,10 +1,7 @@
 function mostrarOcultar(){
-    //Si la caja se ve
     if(caja.style.display == 'block'){
-        //ocultamos la caja
         ocultar();
     }else{
-        //mostramos la caja
         mostrar();
     }
 }
